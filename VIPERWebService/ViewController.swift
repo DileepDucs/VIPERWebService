6a3b7abceb61bec1276c340d642d0a26b977b6e6
Dileep Jaiswal <dileep.ducs@gmail.com>
2021-01-22 22:41:40 +0530

    Initial Commit

---
+++ VIPERWebService/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VIPERWebService
+//
+//  Created by Amrita Jaiswal on 22/01/21.
+//  Copyright © 2021 Dileep Jaiswal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
